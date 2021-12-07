3fe9275d88c94136e5315a2866d190bcd59912fb
yupix <yupi0982@outlook.jp>
2021-12-07 14:08:50 +0900

    chore: pypiに上げる下準備

--- fastapi_misskey/__init__.py
+++ fastapi_misskey/__init__.py
@@ -2,3 +2,10 @@ from .client import *
 from .config import *
 from .exceptions import *
 from .utils import *
+
+__copyright__ = 'Copyright (C) 2021 yupix'
+__version__ = '0.0.1'
+__license__ = 'MIT License'
+__author__ = 'yupix'
+__author_email__ = 'yupi0982@outlook.jp'
+__url__ = 'https://github.com/yupix/fastapi_misskey'
--- setup.py
+++ setup.py
@@ -13,5 +13,16 @@ setup(
     license='MIT',
     install_requires=['fastapi==0.70.0', 'uvicorn==0.15.0', 'aiohttp==3.8.1', 'aiocache==0.11.1'],
     python_requires='>=3.5',
-    url='https://github.com/yupix/fastapi-misskey'
+    url='https://github.com/yupix/fastapi-misskey',
+    keywords='misskey, fastapi',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
