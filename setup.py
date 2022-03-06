e7d671c2f60f91e8e138d3d46113d55df9897e3e
yupix <yupi0982@outlook.jp>
2022-03-06 11:22:10 +0900

    chore: 依存関係の更新

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import find_packages, setup
 
 readme = open('README.md', 'r', encoding='utf-8').read()
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="fastapi_misskey",
     packages=find_packages(),
@@ -11,7 +14,7 @@ setup(
     long_description_content_type="text/markdown",
     author='yupix',
     license='MIT',
-    install_requires=['fastapi==0.70.0', 'uvicorn==0.15.0', 'aiohttp==3.8.1', 'aiocache==0.11.1', 'pydantic==1.8.2'],
+    install_requires=requirements,
     python_requires='>=3.5',
     url='https://github.com/yupix/fastapi-misskey',
     keywords='misskey, fastapi',
